1df71c5a84b143231b9ea9ee7db61702850e1afa
nitikatyagi05 <nitika.tyagi.cse.2025@miet.ac.in>
2026-08-25 20:20:28 +0530

    Add files via upload

---
+++ bubblesort.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n)
+{
+    int i, j, temp;
+
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void display(int arr[], int n)
+{
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+}
+
+int main()
+{
+    int arr[100], n, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements: ");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    bubbleSort(arr, n);
+
+    printf("Sorted array: ");
+    display(arr, n);
+
+    return 0;
+}
